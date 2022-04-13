c666d05a1544fef91312831f6aea516b48bf6cc8
cuong@foxit.co.jp <cuong@foxit.co.jp>
2022-04-13 15:10:39 +0700

    init

---
+++ components/navbar.tsx
@@ -0,0 +1,161 @@
+import React, { useState } from "react";
+import styled, { css } from "styled-components";
+
+import { Input, Badge, Burger } from "@mantine/core";
+import { Search } from "tabler-icons-react";
+import Link from "next/link";
+import Image from "next/image";
+const BurgerStyle = styled(Burger)`
+  display: none;
+  @media only screen and (max-width: 768px) {
+    display: block;
+  }
+`;
+const ImageStyle = styled(Image)`
+  @media only screen and (max-width: 768px) {
+    margin-top: 10px !important;
+  }
+`;
+
+const Wrapper = styled.div`
+  background: #333333;
+  color: #fff;
+  height: auto;
+  padding: 20px 0px;
+  position: fixed;
+  width: 100%;
+  @media only screen and (max-width: 768px) {
+    padding: 5px 0px;
+  }
+`;
+const Content = styled.div`
+  width: 95%;
+  margin: 0 auto;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 20px;
+`;
+const Center = styled.div`
+  display: flex;
+  flex-basis: 350px;
+  flex-direction: column;
+  /* border: 1px solid white; */
+  align-items: space-between;
+  justify-content: center;
+  gap: 5px;
+`;
+
+export const FlexRow = ({}) => css`
+  list-style-type: none;
+  display: flex;
+  margin: 0;
+  padding: 0;
+`;
+export const LiStyle = ({}) => css`
+  /* padding: 0 20px; */
+  margin-left: 20px;
+  :first-child {
+    margin-left: 0px;
+  }
+  /* list-style: none; */
+`;
+
+const Logo = styled.div`
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+`;
+const SearchStyle = styled.div`
+  width: 100%;
+`;
+
+const Menu = styled.ul`
+  ${FlexRow({})};
+  transition: left 400ms ease-in-out;
+  @media only screen and (max-width: 768px) {
+    position: absolute;
+    height: 100vh;
+    background: pink;
+    top: 94px;
+    width: 100%;
+    left: 0;
+    right: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    padding: 0;
+  }
+  /* border: 2px solid white; */
+`;
+const MenuItem = styled.li`
+  ${LiStyle({})};
+
+  width: 100%;
+  a {
+    display: block;
+  }
+  @media only screen and (max-width: 768px) {
+    padding: 10px;
+    margin: 0;
+  }
+`;
+const Control = styled.ul`
+  ${FlexRow({})};
+`;
+const ControlItem = styled.li`
+  ${LiStyle({})};
+`;
+
+const Navbar = () => {
+  const [opened, setOpened] = useState(false);
+  const title = opened ? "Close navigation" : "Open navigation";
+
+  return (
+    <Wrapper>
+      <Content>
+        <Logo>
+          <Link href="/sqdq">
+            <ImageStyle src="/vercel.svg" height={50} width={80} layout="fixed"></ImageStyle>
+          </Link>
+          <BurgerStyle color="gray" opened={opened} onClick={() => setOpened((o) => !o)} title={title} />
+          {/* <div>aa</div> */}
+        </Logo>
+        <Center>
+          <SearchStyle>
+            <Input placeholder="Your twitter" rightSectionWidth={70} styles={{ rightSection: { pointerEvents: "none" } }} rightSection={<Search size={20} color="#000"></Search>} />
+          </SearchStyle>
+          <Menu>
+            <MenuItem>
+              <Link href="/sd">
+                <a>Home</a>
+              </Link>
+            </MenuItem>
+            <MenuItem>
+              <Link href="/">
+                <a>About</a>
+              </Link>
+            </MenuItem>
+            <MenuItem>
+              <Link href="/123">
+                <a>Contact</a>
+              </Link>
+            </MenuItem>
+            <MenuItem>
+              <Link href="/214">
+                <a>Product</a>
+              </Link>
+            </MenuItem>
+          </Menu>
+        </Center>
+        <Control>
+          <ControlItem>Cart</ControlItem>
+        </Control>
+      </Content>
+    </Wrapper>
+  );
+};
+
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,22 @@
-import { Button } from "@mantine/core";
 import styled from "styled-components";
-
+import Image from "next/image";
 const Wrapper = styled.div`
-  display: flex;
+  /* display: flex;
   flex-direction: column;
-  gap: 20px;
+  align-items: flex-start;
+  gap: 20px; */
 `;
 
 function Home({ posts }: { posts: any }) {
-  return (
-    <Wrapper>
-      <Button>TEST</Button>
-      <Button>TEST</Button>
-      <img src="./vercel.svg" style={{ width: "100px" }}></img>
-      <img src="./vercel.svg" style={{ width: "100px" }}></img>
-      <img src="./vercel.svg" style={{ width: "100px" }}></img>
-      <img src="./vercel.svg" style={{ width: "100px" }}></img>
-      <img src="./vercel.svg" style={{ width: "100px" }}></img>
-      <img src="./vercel.svg" style={{ width: "100px" }}></img>
-      <img src="./vercel.svg" style={{ width: "100px" }}></img>
-      <img src="./vercel.svg" style={{ width: "100px" }}></img>
-      <img src="./vercel.svg" style={{ width: "100px" }}></img> <img src="./vercel.svg" style={{ width: "100px" }}></img>
-      <img src="./vercel.svg" style={{ width: "100px" }}></img>
-      <img src="./vercel.svg" style={{ width: "100px" }}></img>
-      <img src="./vercel.svg" style={{ width: "100px" }}></img>
-      <img src="./vercel.svg" style={{ width: "100px" }}></img>
-      <img src="./vercel.svg" style={{ width: "100px" }}></img>
-      <img src="./vercel.svg" style={{ width: "100px" }}></img>
-      <img src="./vercel.svg" style={{ width: "100px" }}></img>
-      <img src="./vercel.svg" style={{ width: "100px" }}></img>
-      <img src="./vercel.svg" style={{ width: "100px" }}></img>
-      <img src="./vercel.svg" style={{ width: "100px" }}></img>
-      <img src="./vercel.svg" style={{ width: "100px" }}></img> <img src="./vercel.svg" style={{ width: "100px" }}></img>
-      <img src="./vercel.svg" style={{ width: "100px" }}></img>
-      <img src="./vercel.svg" style={{ width: "100px" }}></img>
-      <Button>TEST</Button>
-      <Button>TEST</Button>
-      <Button>TEST</Button>
-      <Button>TEST</Button>
-      <Button>TEST</Button>
-      <Button>TEST</Button>
-      <Button>TEST</Button>
-      <Button>TEST</Button>
-      <Button>TEST</Button>
-      <Button>TEST</Button>
-      <Button>TEST</Button>
-    </Wrapper>
-  );
+  return <Wrapper>{/* <Image src="/vercel.svg" alt="Picture of the author" width={50} height={50} style={{ border: "1px solid black!important" }} /> */}</Wrapper>;
 }
-// export async function getStaticProps() {
-//   // Call an external API endpoint to get posts
-//   // const res = await fetch("https://.../posts");
-//   // const posts = await res.json();
-//   const posts = "CUONG";
-//   // By returning { props: { posts } }, the Blog component
-//   // will receive `posts` as a prop at build time
-//   return {
-//     props: {
-//       posts,
-//     },
-//   };
-// }
+export async function getStaticProps() {
+  const posts = "HOME PAGE";
+  return {
+    props: {
+      posts,
+    },
+  };
+}
 
 export default Home;
