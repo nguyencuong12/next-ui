11546b3f935294851d785a978071d1c76ace4d89
cuong@foxit.co.jp <cuong@foxit.co.jp>
2022-04-13 16:32:25 +0700

    add lazy loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import dynamic from "next/dynamic";
-const Navbar = dynamic(() => import("../components/navbar"));
+const Navbar = dynamic(() => import("../components/navbar"), { loading: () => <p>Loading</p> });
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
