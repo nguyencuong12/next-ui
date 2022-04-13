6fa277cdef4af27c8cd130c2745d3b51957a198f
cuong@foxit.co.jp <cuong@foxit.co.jp>
2022-04-13 16:37:37 +0700

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import dynamic from "next/dynamic";
-const Navbar = dynamic(() => import("../components/navbar"), { loading: () => <p>Loading</p> });
+const Navbar = dynamic(() => import("../components/navbar"), { ssr: false, loading: () => <p>LOADING COMPONENTS</p> });
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
