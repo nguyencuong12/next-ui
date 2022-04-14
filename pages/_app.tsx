3f71a701fe8c78f1b65d301e22a05b38150ec819
cuong@foxit.co.jp <cuong@foxit.co.jp>
2022-04-14 10:59:13 +0700

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,18 +5,17 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 const Loading = dynamic(() => import("../components/loading"));
 const Body = dynamic(() => import("../components/body"));
-
 const Navbar = dynamic(() => import("../components/navbar"), { ssr: false, loading: () => <Loading></Loading> });
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <React.Suspense>
       <Navbar></Navbar>
       {/* <Loading></Loading> */}
       <Component {...pageProps} />
       <div style={{ paddingTop: "200px" }}>
         <Body></Body>
       </div>
-    </>
+    </React.Suspense>
   );
 }
 
