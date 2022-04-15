4014f643e9c5d6dbfb1974c84460de4b02bf0b3b
cuong@foxit.co.jp <cuong@foxit.co.jp>
2022-04-15 16:34:19 +0700

    add axios

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,34 @@
 import { useEffect } from "react";
+import axios from "axios";
 
+//
 import dynamic from "next/dynamic";
 function Home({ data }: any) {
   // Render data...
   useEffect(() => {
     console.log("type", typeof window);
+    console.log("DATA", data);
   }, []);
-  return <div>CUONG</div>;
+  return (
+    <div style={{ paddingTop: "200px" }}>
+      <h1>{data.count}</h1>
+      CUONG
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  // const res = await fetch(`https://.../data`);
+  let response = await axios.get("https://api.sashimeomeo.com/product");
+  // Fetch data from external API
+  const data = response.data.products;
+  // Pass data to the page via props
+
+  // const data = await res.json();
+
+  // Pass data to the page via props
+  return { props: { data } };
 }
 
 // This gets called on every request
