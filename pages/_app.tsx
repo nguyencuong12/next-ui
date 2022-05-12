8ce320d658c1954330546b8b2e503ad84bf66829
Nguyen Cuong <cuong@foxit.co.jp>
2022-05-12 20:23:04 +0700

    add not scale

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,27 @@
-import dynamic from "next/dynamic";
+import dynamic from 'next/dynamic';
 // import { Suspense, FC, lazy } from "react";
-import React, { useEffect } from "react";
-import "../styles/globals.css";
-import Head from "next/head";
-import styled, { ThemeProvider } from "styled-components";
-import type { AppProps } from "next/app";
+import React, { useEffect } from 'react';
+import '../styles/globals.css';
+import Head from 'next/head';
+import styled, { ThemeProvider } from 'styled-components';
+import type { AppProps } from 'next/app';
 
-import { Button, MantineProvider } from "@mantine/core";
-import NextNProgress from "nextjs-progressbar";
-import { store } from "../redux/store";
-import { Provider } from "react-redux";
-import { layoutVariables } from "../styles/variable.global";
+import { Button, MantineProvider } from '@mantine/core';
+import NextNProgress from 'nextjs-progressbar';
+import { store } from '../redux/store';
+import { Provider } from 'react-redux';
+import { layoutVariables } from '../styles/variable.global';
 
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import { useTranslation } from "react-i18next";
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
-const Navbar = dynamic(() => import("../components/navbar"));
-const Footer = dynamic(() => import("../components/footer"));
-const Body = dynamic(() => import("../components/body"));
+const Navbar = dynamic(() => import('../components/navbar'));
+const Footer = dynamic(() => import('../components/footer'));
+const Body = dynamic(() => import('../components/body'));
 
-import vietnam from "../locates/vietnam.json";
-import english from "../locates/english.json";
+import vietnam from '../locates/vietnam.json';
+import english from '../locates/english.json';
 const Layout = styled.div`
   position: relative;
   top: 0;
@@ -29,7 +29,7 @@ const Layout = styled.div`
   height: auto;
 `;
 const theme = {
-  main: "mediumseagreen",
+  main: 'mediumseagreen',
 };
 const WrapperLoading = styled.div`
   width: 100%;
@@ -54,7 +54,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "vn", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    lng: 'vn', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
 
@@ -64,15 +64,24 @@ i18n
   });
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    localStorage.setItem("language", "vn");
+    localStorage.setItem('language', 'vn');
   }, []);
 
   return (
     <Provider store={store}>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
+      </Head>
       <NextNProgress></NextNProgress>
       <Head>
         <title>Sashimeomeo Shop</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
       <ThemeProvider theme={layoutVariables}>
         <MantineProvider withGlobalStyles withNormalizeCSS>
