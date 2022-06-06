bfa0280f379ce24394638edae3f8c52b83537508
nguyencuong12 <ffgcuong@gmail.com>
2022-06-06 09:10:12 +0700

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,28 @@
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 // import { Suspense, FC, lazy } from "react";
-import React, { useEffect } from 'react';
-import '../styles/globals.css';
-import Head from 'next/head';
-import styled, { ThemeProvider } from 'styled-components';
-import type { AppProps } from 'next/app';
+import React, { useEffect } from "react";
+import "../styles/globals.css";
+import Head from "next/head";
+import styled, { ThemeProvider } from "styled-components";
+import type { AppProps } from "next/app";
 
-import { Button, MantineProvider } from '@mantine/core';
-import NextNProgress from 'nextjs-progressbar';
-import { store } from '../redux/store';
-import { Provider } from 'react-redux';
-import { layoutVariables } from '../styles/variable.global';
+import { Button, MantineProvider } from "@mantine/core";
+import NextNProgress from "nextjs-progressbar";
+import { store } from "../redux/store";
+import { Provider } from "react-redux";
+import { layoutVariables } from "../styles/variable.global";
 
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import { useTranslation } from 'react-i18next';
-import { useRouter } from 'next/router';
-const Navbar = dynamic(() => import('../components/navbar'));
-const Footer = dynamic(() => import('../components/footer'));
-const Body = dynamic(() => import('../components/body'));
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import { useTranslation } from "react-i18next";
+import { useRouter } from "next/router";
+const Navbar = dynamic(() => import("../components/navbar"));
+const Footer = dynamic(() => import("../components/footer"));
+const Body = dynamic(() => import("../components/body"));
 
-import vietnam from '../locates/vietnam.json';
-import english from '../locates/english.json';
-
+import vietnam from "../locates/vietnam.json";
+import english from "../locates/english.json";
+import Script from "next/script";
 const Layout = styled.div`
   position: relative;
   top: 0;
@@ -30,7 +30,7 @@ const Layout = styled.div`
   height: auto;
 `;
 const theme = {
-  main: 'mediumseagreen',
+  main: "mediumseagreen",
 };
 const WrapperLoading = styled.div`
   width: 100%;
@@ -55,7 +55,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: 'vn', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    lng: "vn", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
 
@@ -63,33 +63,56 @@ i18n
       escapeValue: false, // react already safes from xss
     },
   });
+
+{
+  /*
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-C07RQ395DP"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-C07RQ395DP');
+</script> 
+
+
+*/
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    localStorage.setItem('language', 'vn');
+    localStorage.setItem("language", "vn");
   }, []);
 
   return (
-    <Provider store={store}>
-      <NextNProgress></NextNProgress>
-      <Head>
-        <title>Sashimeomeo Shop</title>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-        />
-      </Head>
-      <ThemeProvider theme={layoutVariables}>
-        <MantineProvider withGlobalStyles withNormalizeCSS>
-          <Layout>
-            <Navbar></Navbar>
-            <Body>
-              <Component {...pageProps} />
-            </Body>
-            <Footer></Footer>
-          </Layout>
-        </MantineProvider>
-      </ThemeProvider>
-    </Provider>
+    <>
+      <Script strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=G-C07RQ395DP"></Script>
+      <Script strategy="lazyOnload">
+        {` window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-C07RQ395DP');`}
+      </Script>
+      <Provider store={store}>
+        <NextNProgress></NextNProgress>
+        <Head>
+          <title>Sashimeomeo Shop</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+        </Head>
+        <ThemeProvider theme={layoutVariables}>
+          <MantineProvider withGlobalStyles withNormalizeCSS>
+            <Layout>
+              <Navbar></Navbar>
+              <Body>
+                <Component {...pageProps} />
+              </Body>
+              <Footer></Footer>
+            </Layout>
+          </MantineProvider>
+        </ThemeProvider>
+      </Provider>
+    </>
   );
 }
 
